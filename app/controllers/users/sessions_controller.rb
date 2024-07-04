81a64b50483f156ff007dce5aaf771242eb16314
Karol Zerna <65570079+KarolZerna@users.noreply.github.com>
2024-07-04 08:41:49 -0400

    fix bug session (#12)

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -19,22 +19,32 @@ class Users::SessionsController < Devise::SessionsController
   # rubocop:enable Lint/UnusedMethodArgument
 
   def respond_to_on_destroy
-    jwt_payload = JWT.decode(
-      request.headers['Authorization'].split(' ')[1],
-      Rails.application.credentials.fetch(:secret_key_base)
-    ).first
+    begin
+      token = request.headers['Authorization']&.split(' ')&.last
+      raise 'Token not found' unless token
 
-    current_user = User.find(jwt_payload['sub'])
+      jwt_payload = JWT.decode(
+        token,
+        Rails.application.credentials.fetch(:secret_key_base)
+      ).first
 
-    if current_user
-      render json: {
-        status: 200,
-        message: 'Signed out successfully'
-      }, status: :ok
-    else
+      current_user = User.find(jwt_payload['sub'])
+
+      if current_user
+        render json: {
+          status: 200,
+          message: 'Signed out successfully'
+        }, status: :ok
+      else
+        render json: {
+          status: 401,
+          message: 'User has no active session'
+        }, status: :unauthorized
+      end
+    rescue JWT::DecodeError, ActiveRecord::RecordNotFound, RuntimeError => e
       render json: {
         status: 401,
-        message: 'User has no active session'
+        message: e.message
       }, status: :unauthorized
     end
   end
